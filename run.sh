4390e486805fb4ecf59309aa4eea6bd6af0e061f
Thierry Kauffmann <thierry.kauffmann@arawa.fr>
2024-07-28 17:08:01 +0200

    Minor change

--- run.sh
+++ run.sh
@@ -14,10 +14,10 @@ script_path=$1
 
 
 if [[ "$runtime" = "official" ]]; then
-    docker build --quiet --tag php:concurrency --build-arg PUID=$(id -u) --build-arg PGID=$(id -g) --build-arg USER=$(id -un) .
+    docker build --quiet --tag php:concurrency --build-arg PUID=$(id -u) --build-arg PGID=$(id -g) --build-arg USER=$(id -un) ./Dockerfiles/official
     time docker run --rm -v $(pwd):/app/ php:concurrency php /app/$script_path | tee out.log
 elif [[ "$runtime" = "frankenphp" ]]; then
-    docker build --quiet --tag frankenphp:concurrency --build-arg PUID=$(id -u) --build-arg PGID=$(id -g) --build-arg USER=$(id -un) ./frankenphp
+    docker build --quiet --tag frankenphp:concurrency --build-arg PUID=$(id -u) --build-arg PGID=$(id -g) --build-arg USER=$(id -un) ./Dockerfiles/frankenphp
     time docker run --rm -v $(pwd):/app/public frankenphp:concurrency frankenphp php-cli /app/public/$script_path  | tee out.log
     #docker container rm -f franken-concurrency
     #docker run  --name franken-concurrency -v $(pwd):/app/public -p 8080:80 -p 8443:443 -p 8443:443/udp frankenphp:concurrency

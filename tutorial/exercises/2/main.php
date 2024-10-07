15658bbea74575a1b840b2915037ed46c4246b5a
Thierry Kauffmann <thierry.kauffmann@arawa.fr>
2024-10-07 20:23:21 +0200

    Update exercise 2

--- tutorial/exercises/2/main.php
+++ tutorial/exercises/2/main.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Use the given function as a task and launch at least 3 parallel tasks with it
+ * which will be run one after the other in the same parallel thread.
  * 
  * Hint :
  * \parallel\run(Closure $task, array $argv): ?Future
--- tutorial/solutions/2/main.php
+++ tutorial/solutions/2/main.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Use the given function as a task and launch at least 3 parallel tasks with it
- * 
+ * which will be run one after the other in the same parallel thread.
  * Hint :
  * \parallel\run(Closure $task, array $argv): ?Future
  * Shall schedule task for execution in parallel, passing argv at execution time. 
@@ -15,6 +15,8 @@ $task = function (string $message, int $sleep_time) {
 
 $data = [["Hello", 1], ["Halo", 3], ["Hej", 2]];
 
+$runtime = new \parallel\Runtime();
+
 foreach ($data as $datum) {
-    \parallel\run($task, $datum);
+    $runtime->run($task, $datum);
 }
